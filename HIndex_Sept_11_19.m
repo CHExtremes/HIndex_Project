4376558961e9fe9514e601165d306fdf935c7cdd
mstone2232 <54993995+mstone2232@users.noreply.github.com>
2019-09-11 14:21:23 -0500

    Sept. 11 2019 update 2
    Added a section 7 to calculate C-Index values

--- HIndex_Sept_11_19.m
+++ HIndex_Sept_11_19.m
@@ -94,3 +94,58 @@ plot(x,y1)
 hold on
 plot(x,y2)
 %barh(x,y)
+
+%% 7 
+%create a C-Index
+clc
+
+
+%stationLength = 1;
+ %calls the path of the current file directory
+CIndex = (1890:2019)';
+for i = 1:stationLength %for each station
+    baseFileName = stationNames(i); %this is the name of the file excluding file type. 
+    fullFileName = fullfile(folder, baseFileName); %creates a variable for the full file path to ensure no errors related to file path
+    temporaryFile = readmatrix(fullFileName); %creates a temporary matrix of the the data for the current station name.
+    yearColumn = 1;
+    dayColumn = 3;
+    TMaxColumn = 4;
+    TMinColumn = 5;
+    %creates an array from the starting year to the ending year of the stations available weather data
+    temporaryCIndex = transpose(min(temporaryFile(:,yearColumn)):max(temporaryFile(:,yearColumn))); %creates an column array for the years of the H-Indecies 
+    counter = 0;
+    for j = temporaryCIndex(1,1):temporaryCIndex(end,1)%for each year at this station
+        year = find(temporaryFile==j); %locates the index values for the given year
+        B = temporaryFile(year,:); %creates a temporary matrix for the given year
+        currentTemp = round(min(B(:,TMinColumn))); %records max temp for the given year 
+        
+        %Count the number of times where the daily temp is greater than or
+                %equal to that temp
+        while counter < 32-currentTemp %checks to see if the counter is smaller than the currentTemp. This is to make sure that the value is an H-Index value.
+            counter = 0;
+            for h = 1:length(B)% for days in this year
+                if B(h,TMinColumn) <= currentTemp %If the value at row h and column TMaxColumn are greater than currentTemp
+                    counter = counter + 1; %increase counter by 1
+                end    
+            end
+            if counter < 32-currentTemp %if the counter is smaller than currentTemp then the H-index is not valid, so we reduce it by one and repeat the loop.
+                currentTemp = currentTemp + 1;
+            end
+        end
+        temporaryCIndex((j-temporaryCIndex(1,1)+1), 2) = currentTemp; %stores the max H-index value for each year in temporaryHIndex      
+    end
+    
+    for j = 1:length(temporaryCIndex) %for the number of years at the current station
+        for h = 1:length(CIndex) %for full array of years being analyzed
+           if temporaryCIndex(j,1) == CIndex(h,1) %Checks to make sure that the years are the same for the given station
+              CIndex(h,i+1)=temporaryCIndex(j,2); %if the years are the same, then the yearly value for the station is stored in a column specifically for that station
+           end
+        end
+    end
+end
+x = CIndex(:,1);
+y1 = CIndex(:,2);
+y2 = CIndex(:,3);
+plot(x,y1)
+hold on
+plot(x,y2)
