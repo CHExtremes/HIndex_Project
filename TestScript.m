c8a91242ab9851f6b71f88b10bbecd3757997e06
Matt Stone <mstone2232@ksu.edu>
2019-10-11 12:54:58 -0500

    Second line of text added

--- TestScript.m
+++ TestScript.m
@@ -1,2 +1,5 @@
 %I'm just gonna write a few lines here to see how the itteration of this
 %works in github.
+
+%this is a second line added to see if I can revert to old versions after
+%this edit is commited
