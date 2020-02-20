77386093c8ed3067bfe2e7135b368c3a66ffec0c
Matt Stone <mstone2232@ksu.edu>
2020-02-20 10:56:01 -0600

    This is the last version before W-index and the Peroid of Record was removed from HIndex_main

--- scripts/HIndex_Main.m
+++ scripts/HIndex_Main.m
@@ -52,6 +52,7 @@ for i = 1:stationLength %for each station
     baseFileName = stationNames(i); %this is the name of the file excluding file type. 
     fullFileName = fullfile(folder, baseFileName); %creates a variable for the full file path to ensure no errors related to file path
     temporaryFile = readtable(fullFileName); %creates a temporary matrix of the the data for the current station name.
+    temporaryFile.TMAX = round(temporaryFile.TMAX,0);
     %creates an array from the starting year to the ending year of the stations available weather data
 %     commented out this section of script for the same reason as above.
 %     if useStartYear == 1 && useStopYear == 0
@@ -184,7 +185,7 @@ for i = 1:length(tableStationNames) %for each station in station names
     end
     
     ylim([79 95]) %sets boundries for the y axis for all graphs
-    
+    %ylim([55 72])
     if i == 1 || i == 7 || i == 13 || i == 19
         ylabel('H - Index (days/year)', 'FontSize', 11)
         yticks('auto')
@@ -242,7 +243,7 @@ for i = 1:length(tableStationNames) %for each station in station names
     HResults.maxSP(i) = max(B.HIndex);
     HResults.meanSP(i) = round(mean(B.HIndex),1);
     HResults.medianSP(i) = median(B.HIndex);
-    title(compose(tempNames(2,1)+"\n"+HResults.slopePOR(i)*100+"\n"+HResults.slopeSP(i)*100),'FontSize', 11);
+    title(compose(tempNames(2,1)+"\n"+num2str(HResults.slopePOR(i)*100,"%#.1f")+"\n"+num2str(HResults.slopeSP(i)*100,"%#.1f")),'FontSize', 11);
 end
 %use the below script when you want to automatically make tiff files for
 %the given graphs. There is an issue with this function in that it doesn't
@@ -268,6 +269,7 @@ for i = 1:stationLength %for each station
     baseFileName = stationNames(i); %this is the name of the file excluding file type. 
     fullFileName = fullfile(folder, baseFileName); %creates a variable for the full file path to ensure no errors related to file path    
     temporaryFile = readtable(fullFileName); %creates a temporary matrix of the the data for the current station name.
+    temporaryFile.TMIN = round(temporaryFile.TMIN,0);
     %creates an array from the starting year to the ending year of the stations available weather data
     YEAR = transpose(min(temporaryFile.YEAR):max(temporaryFile.YEAR));
     temporaryCIndex = table(YEAR); %creates an column array for the years of the H-Indecies 
@@ -440,7 +442,7 @@ for i = 1:length(tableStationNames) %for each station in station names
     CResults.maxSP(i) = max(B.CIndex);
     CResults.meanSP(i) = round(mean(B.CIndex),1);
     CResults.medianSP(i) = median(B.CIndex);
-    title(compose(tempNames(2,1)+"\n"+100*CResults.slopePOR(i)+"\n"+100*CResults.slopeSP(i)),'FontSize', 11);
+    title(compose(tempNames(2,1)+"\n"+num2str(100*CResults.slopePOR(i),"%#.1f")+"\n"+num2str(100*CResults.slopeSP(i),"%#.1f")),'FontSize', 11);
 end
 %use the below script when you want to automatically make tiff files for
 %the given graphs. See H-Index for issues with this function
@@ -455,19 +457,24 @@ end
 clc
 tic
 %stationLength = 1;
- %calls the path of the current file directory
+%calls the path of the current file directory
 stationLength = length(stationNames);
 WIndex = zeros(2013-1890+1,23);
 WIndex(:,1) = (1890:2013)';
+useInches = 0;
 for i = 1:stationLength %for each station
     baseFileName = stationNames(i); %this is the name of the file excluding file type. 
     fullFileName = fullfile(folder, baseFileName); %creates a variable for the full file path to ensure no errors related to file path    
     temporaryFile = readtable(fullFileName); %creates a temporary matrix of the the data for the current station name.
+    temporaryFile.RAIN = round(temporaryFile.RAIN,0); %rounds the result to be a whole number, since we don't have measure values for mm to the tenthousandths place.
+    if useInches == 1        
+        temporaryFile.RAIN = round(temporaryFile.RAIN/25.4,2); %converts precipitations values from mm to inches
+    end
     %creates an array from the starting year to the ending year of the stations available weather data
     YEAR = transpose(min(temporaryFile.YEAR):max(temporaryFile.YEAR));
     temporaryWIndex = table(YEAR); %creates an column array for the years of the H-Indecies 
     temporaryWIndex.WIndex = zeros(height(temporaryWIndex),1);
-    counter = 0;
+    counter = 1;
     for j = temporaryWIndex.YEAR(1):temporaryWIndex.YEAR(end)%for each year at this station
         year = temporaryFile(temporaryFile.YEAR==j,:); %locates the index values for the given year and creates a temporary matrix for the given year       
         currentPrecip = max(year.RAIN); %records max precip for the given year        
@@ -477,14 +484,26 @@ for i = 1:stationLength %for each station
             counter = 0;
             for h = 1:height(year)% for days in this year
                 if year.RAIN(h) >= currentPrecip %If the value at row h and column TMaxColumn are greater than currentTemp
-                    counter = counter + 1; %increase counter by 1
+                   if useInches == 1
+                       counter = counter + 0.01;
+                   else
+                       counter = counter + 1;
+                   end %increase counter by 1
                 end    
             end
             if counter < currentPrecip %if the counter is smaller than currentTemp then the H-index is not valid, so we reduce it by one and repeat the loop.
-                currentPrecip = currentPrecip - 1 ;
+                if useInches == 1
+                    currentPrecip = currentPrecip - 0.01;
+                else
+                    currentPrecip = currentPrecip - 1 ;
+                end
             end
         end
-        temporaryWIndex.WIndex(j-temporaryWIndex.YEAR(1)+1) = currentPrecip; %stores the max H-index value for each year in temporaryHIndex      
+        if useInches == 1
+            temporaryWIndex.WIndex(j-temporaryWIndex.YEAR(1)+1) = currentPrecip*100; %stores the max H-index value for each year in temporaryHIndex
+        else
+            temporaryWIndex.WIndex(j-temporaryWIndex.YEAR(1)+1) = currentPrecip;
+        end
     end
     
     for j = 1:height(temporaryWIndex) %for the number of years at the current station
@@ -495,6 +514,7 @@ for i = 1:stationLength %for each station
         end
     end  
 end
+
 timeWIndex = toc;
 %%
 WResults = table;
@@ -505,7 +525,7 @@ WResults.NAME = tableStationNames';
 % useStartYear = 0;
 % useStopYear = 0;
 figure('Name', 'W-Index')
-%periods = 
+%formatSpec = "%f";
 for i = 1:length(tableStationNames) %for each station in station names 
     %this section creates an array of subplots where each station has its
     %own plot showing the data points in orange, the change in time in
@@ -513,7 +533,7 @@ for i = 1:length(tableStationNames) %for each station in station names
     tempNames = split(tableStationNames(i), '_'); %assigns a variable to the stations name and cuts out any unnecissary labels used for organization purposes
     A = array2table(WIndex(WIndex(:,1+i) ~= 0,[ 1 1+i])); %for each station, create a new array with the non-zero values and their corresponding years
     A.Properties.VariableNames{'Var1'} = 'YEAR'; 
-    A.Properties.VariableNames{'Var2'} = 'WIndex';  
+    A.Properties.VariableNames{'Var2'} = 'WIndex';
     B = A(A.YEAR>=startYear & A.YEAR <=stopYear,:);
     
     subplot(4,6,i) %Creates a system of subplots in a 4x6 grid
@@ -582,8 +602,12 @@ for i = 1:length(tableStationNames) %for each station in station names
     else
         ylabel('')
         yticks('')
-    end  
-    ylim([5 26])
+    end
+    if useInches == 1
+       ylim([15 46]) 
+    else
+       ylim([5 26])
+    end
     
     %this section  creates a table statistically important values for both
     %the POR and the specified period
@@ -601,7 +625,11 @@ for i = 1:length(tableStationNames) %for each station in station names
     [r,p3]=corr(xA,yA);%pearson (linear) method
     r_pA(i,1:3)=[i,r,p3]; %pearson(Least square method) method corrcoef(x,y);    
     
-    WResults.slopePOR(i) = round(table2array(mdlA.Coefficients(2,1)),3);%calls the slope given for the linear regression of the data using the fitlm function
+    if useInches == 1
+        WResults.slopePOR(i) = round(table2array(mdlA.Coefficients(2,1)),3);
+    else
+        WResults.slopePOR(i) = round(table2array(mdlA.Coefficients(2,1)),3);%calls the slope given for the linear regression of the data using the fitlm functioN
+    end%calls the slope given for the linear regression of the data using the fitlm function
     WResults.rPOR(i) = r_pA(i,2);
     WResults.rSqrPOR(i) = mdlA.Rsquared.Ordinary; %calls the r^2 value from the fitlm function and inputs it into a new table
     WResults.rPValuePOR(i) = round(table2array(mdlA.Coefficients(2,4)),3); %calls the pValue from the fitlm function and inputs it into a new table
@@ -623,7 +651,11 @@ for i = 1:length(tableStationNames) %for each station in station names
     [r,p3]=corr(xB,yB);%pearson (linear) method
     r_pB(i,1:3)=[i,r,p3]; %pearson(Least square method) method corrcoef(x,y); 
     
-    WResults.slopeSP(i) = round(table2array(mdlB.Coefficients(2,1)),3);%calls the slope given for the linear regression of the data using the fitlm functioN
+    if useInches == 1
+        WResults.slopeSP(i) = round(table2array(mdlB.Coefficients(2,1)),3, 'significant');
+    else
+        WResults.slopeSP(i) = round(table2array(mdlB.Coefficients(2,1)),3, 'significant');%calls the slope given for the linear regression of the data using the fitlm functioN
+    end
     WResults.rSP(i) = r_pB(i,2);
     WResults.rSqrSP(i) = mdlB.Rsquared.Ordinary; %calls the r^2 value from the fitlm function and inputs it into a new table
     WResults.rPValueSP(i) = round(table2array(mdlB.Coefficients(2,4)),3); %calls the pValue from the fitlm function and inputs it into a new table
@@ -635,129 +667,11 @@ for i = 1:length(tableStationNames) %for each station in station names
     WResults.maxSP(i) = max(B.WIndex);
     WResults.meanSP(i) = round(mean(B.WIndex),1);
     WResults.medianSP(i) = median(B.WIndex);
-    title(compose(tempNames(2,1)+"\n"+100*WResults.slopePOR(i)+"\n"+100*WResults.slopeSP(i)),'FontSize', 11);
+    %num2str(100*WResults.slopePOR(23), "%#g")
+    title(compose(tempNames(2,1)+"\n"+num2str(100*WResults.slopePOR(i),"%#.1f")+"\n"+num2str(100*WResults.slopeSP(i),"%#.1f")),'FontSize', 11);
 end
 %use the below script when you want to automatically make tiff files for
 %the given graphs. See H-Index for issues with this function
 %set(gcf,'PaperPositionMode','auto') %set the print area same as paper
 %print('-dtiff','-r600', 'W_Index_Subplots')
 
-%% 5
-%Writes a dry index, D-Index. This could be done by counting x number of
-%periods that had x number of days with no rain. I don't think that will be
-%very informative since prolonged dry periods(drought) are more likely to
-%be influential than knowning that there are more, shorter dry periods.
-clc
-tic
-%stationLength = 1;
- %calls the path of the current file directory
-stationLength = length(stationNames);
-DIndex = zeros(stopYear-startYear+1,23);
-DIndex(:,1) = (startYear:stopYear)';
-for i = 1:stationLength %for each station
-    baseFileName = stationNames(i); %this is the name of the file excluding file type. 
-    fullFileName = fullfile(folder, baseFileName); %creates a variable for the full file path to ensure no errors related to file path    
-    temporaryFile = readtable(fullFileName); %creates a temporary matrix of the the data for the current station name.
-    %creates an array from the starting year to the ending year of the stations available weather data
-    if useStartYear == 1 && useStopYear == 0
-        YEAR = transpose(startYear:max(temporaryFile.YEAR));
-    elseif useStartYear == 1 && useStopYear == 1
-        YEAR = transpose(startYear:stopYear);
-    elseif useStartYear == 0 && useStopYear == 1
-        YEAR = transpose(min(temporaryFile.YEAR):stopYear);
-    else     
-        YEAR = transpose(min(temporaryFile.YEAR):max(temporaryFile.YEAR));
-    end
-    temporaryDIndex = table(YEAR); %creates an column array for the years of the H-Indecies 
-    temporaryDIndex.DIndex = zeros(height(temporaryDIndex),1);
-    counter = 0;
-    dryness = 1; %dryness is the minimum precipitation threshold for a day to be considered 'wet'
-    for j = temporaryDIndex.YEAR(1):temporaryDIndex.YEAR(end)%for each year at this station
-            year = temporaryFile(temporaryFile.YEAR==j,:); %locates the index values for the given year and creates a temporary matrix for the given year       
-            currentDryPeriod = 0;
-            tempDryPeriod = 0;
-            counter = 0;
-            for D = 1:height(year) %this loop identifies the longest dry period for the given year               
-                if year.RAIN(D) < dryness
-                    tempDryPeriod = tempDryPeriod + 1;
-                else
-                     if tempDryPeriod > currentDryPeriod
-                         currentDryPeriod = tempDryPeriod;
-                     end
-                     tempDryPeriod = 0;
-                end          
-            end
-            
-            while counter < currentDryPeriod
-                counter = 0;
-                tempDryPeriod = 0;
-                for h = 1:height(year)% for days in this year
-                    
-                    if year.RAIN(h) < dryness
-                        tempDryPeriod = tempDryPeriod + 1;
-                    else
-                        if tempDryPeriod >= currentDryPeriod
-                            counter = counter + 1;
-                        end
-                        tempDryPeriod = 0;
-                    end                    
-                end
-                if counter < currentDryPeriod
-                    currentDryPeriod = currentDryPeriod-1;
-                end
-            end
-            temporaryDIndex.DIndex(j-temporaryDIndex.YEAR(1)+1) = currentDryPeriod;       
-    end
-    
-    for j = 1:height(temporaryDIndex) %for the number of years at the current station
-        for h = 1:length(DIndex) %for full array of years being analyzed
-           if temporaryDIndex.YEAR(j) == DIndex(h,1) %Checks to make sure that the years are the same for the given station
-              DIndex(h,i+1)=temporaryDIndex.DIndex(j); %if the years are the same, then the yearly value for the station is stored in a column specifically for that station
-           end
-        end
-    end  
-end
-timeDIndex = toc;
-
-figure('Name', 'D-Index')
-for i = 1:stationLength %for each station in station names
-    %this section creates an array of subplots where each station has it
-    A = array2table(DIndex(DIndex(:,1+i) ~= 0,[ 1 1+i])); %for each station, create a new array with the non-zero values and their corresponding years
-    A.Properties.VariableNames{'Var1'} = 'YEAR'; 
-    A.Properties.VariableNames{'Var2'} = 'DIndex';
-    subplot(4,6,i)
-    lA = plot(A.YEAR,A.DIndex); %adds a line to the plot for additional clarity
-    hold on %add each station to the same plot
-    mdlA = fitlm(A, 'DIndex ~ YEAR'); %performs a linear regression for the Year and the D Index
-    zA = plot(mdlA); %plots the linear regression and data points
-    %below are changes to the colors and markers of the plot for additional
-    %clarity I removed 95% error bars (z(3) and z(4) to make the graph less cluttered. We
-    %can turn these on later if we want to visually analyze the error
-    %margins.
-    zA(1).Color = '#D95319'; %sets data points to be orange
-    zA(1).Marker = '.';
-    zA(1).MarkerSize = 10;
-    zA(2).Color = 'k';
-    zA(2).LineWidth = 1;
-    zA(3).Color = 'none';
-    zA(4).Color = 'none';
-    legend('off'); %hides the automatic legend generated by fitlm  
-    
-    xlabel('Year', 'FontSize', 11)
-    xlim([startYear (stopYear+1)])
-    ylabel('D - Index', 'FontSize', 11)
-    ylim([5 15])
-    %this section  creates a table of the station names and the slope for
-    %each trend line
-    
-    
-    HResults.DIndexSlope(i) = round(table2array(mdlA.Coefficients(2,1)),3); %calls the slope given for the linear regression of the data using the fitlm function
-    HResults.DIndexAverage(i) = round(mean(A.DIndex),1); %calculates the average D index for the station and adds that to a new table
-    HResults.DIndexRsqr(i) = mdlA.Rsquared.Ordinary; %calls the r^2 value from the fitlm function and inputs it into a new table
-    HResults.DIndexPValue(i) = round(table2array(mdlA.Coefficients(2,4)),3); %calls the pValue from the fitlm function and inputs it into a new table
-    title(compose(tempNames(2,1)+"\n"+HResults.DIndexAverage(i)+"\n"+HResults.DIndexSlope(i)), 'FontSize', 11);
-end
-%use the below script when you want to automatically make tiff files for
-%the given graphs. See H-Index for issues with this function
-%set(gcf,'PaperPositionMode','auto') %set the print area same as paper
-%print('-dtiff','-r600', 'D_Index_Subplots')
