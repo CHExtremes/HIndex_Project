777a66a115ad062812fe94178f7bd3361d00f1db
mstone2232 <54993995+mstone2232@users.noreply.github.com>
2020-04-28 11:13:48 -0500

    Began writing script to run all indices similtaneously

--- TestScript.m
+++
@@ -1,3 +0,0 @@
-The test script not contains text
-Verifying ability to do source control from Matlab.
-Test 2
--- scripts/Test.m
+++
@@ -1,9 +0,0 @@
-
-m = 4;
-n = 6;
-
-for i = 23
-    for j = 23
-    subplot('position',[m+i     
-    end
-end
